7e738f71ecd4e498a1ae13268017771ba7319ea7
W4ff1e <72278085+W4ff1e@users.noreply.github.com>
2024-04-27 19:09:37 -0400

    Removed redudant code, added comments

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@
 mod hangmangame;
 use std::default;
 
-use eframe::egui::{self, Context};
+use eframe::egui::{self};
 use hangmangame::HangmanGameState;
 
 fn main() {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
+    // Define the options for the native application
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default().with_inner_size([1280.0, 720.0]),
         ..Default::default()
@@ -21,45 +22,6 @@ fn main() {
         Box::new(|cc| Box::new(HangmanApp::new(cc))),
     )
     .expect("Failed to run native application! Panic!");
-
-    /* println!("Welcome to Hangman!");
-
-    loop {
-        println!("Please enter your phrase: ");
-
-        // Declare an empty string for the buffer to fill.
-        let mut phrase_to_guess: String = String::new();
-
-        //  Read terminal input
-        stdin()
-            .read_line(&mut phrase_to_guess)
-            .expect("Failed to read terminal input!");
-
-        // Make phrase to guess immutable for this iteration.
-        let phrase_to_guess = phrase_to_guess;
-
-        loop {
-            println!("Please enter a letter to guess:");
-            let mut letter_to_guess: String = String::new();
-
-            stdin()
-                .read_line(&mut letter_to_guess)
-                .expect("Failed to read line!");
-
-            let letter_to_guess: char = match letter_to_guess.trim().parse() {
-                Ok(char) => char,
-                Err(_) => continue,
-            };
-
-            for character in 0..phrase_to_guess.chars().count() {
-                if letter_to_guess == phrase_to_guess.chars().nth(character).unwrap() {
-                    println!("Match!")
-                } else {
-                    println!("No Match!");
-                }
-            }
-        }
-    } */
 }
 
 #[derive(Default)]
